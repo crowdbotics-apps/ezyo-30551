7e794a8ef15c35421ef4dd52d25706819134bd12
Crowdbotics <team@crowdbotics.com>
2021-09-15 00:12:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ezyo_30551Identifier",
+  url: "https://ezyo_30551Identifier.botics.co",
+  api: "https://ezyo_30551Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
